748bf15dc9406aefaf6ef22286d99ae3aff8f126
W1ckh3ad <69201877+W1ckh3ad@users.noreply.github.com>
2020-10-27 16:23:00 +0100

    1

---
+++ src/classes/BasketArrayList.java
@@ -0,0 +1,45 @@
+package classes;
+
+import java.util.ArrayList;
+
+public class BasketArrayList {
+    private ArrayList<String> basket = new ArrayList<>();
+    private int count;
+
+    public BasketArrayList(String... obst) throws Exception {
+        if (obst.length > 10) {
+            throw new Exception();
+        }
+        count = obst.length;
+        for (int i = 0; i < obst.length; i++) {
+            basket[i] = obst[i];
+        }
+    }
+
+    public void addObst(String obst) throws Exception {
+        if (count >= 10) {
+            throw new Exception();
+        }
+        basket[count++] = obst;
+    }
+
+    public void removeObst(int index){
+        count--;
+        for (int i = index + 1; i < basket.length; i++) {
+            basket[i - 1]  = basket[i];
+        }
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        String string = "Obstliste: \n\n";
+        for (int i = 0; i < basket.length; i++) {            
+                if (basket[i] != null) {
+                    string += i + ") " + basket[i] + "\n";            
+                }
+        }
+        string += "Es sind noch " + (10 - count) + " Plätze frei im Obstkorb \n\n\n";
+        return string;
+    }
+}
